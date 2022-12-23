2ca84751262b8556570300a4b045852b0bf3172c
paul <paul4145irvin@gmail.com>
2022-12-23 15:08:12 +0800

    renew

--- src/components/App/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './App.styled';
-export * from './App';
--- src/useContext/shopContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { ShopState, initialState } from '../useReducer';
-
-export const ClothingShopContext = createContext<ShopState>(initialState);
